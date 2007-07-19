c15e1bdd620dcb8886adb3e93f12e5018a70b2df
Chris Prather <chris@prather.org>
2007-07-19 23:34:01 +0000

    move everything into trunk

---
+++ lib/Bundle/Bender.pm
@@ -0,0 +1,49 @@
+package Bundle::Bender;
+use strict;
+use vars qw($VERSION);
+
+$VERSION = '1.0';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Bender - Bundle to install all Bender related Modules
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::Bender'
+
+=head1 DESCRIPTION
+
+Bundle::Bender - Bundle to install all Bender's dependencies.
+
+=head1 CONTENTS
+
+Moose 0.20
+
+MooseX::Storage 0.01
+
+POE 0.9989
+
+POE::Component::IRC 5.30
+
+POE::Component::Generic 0.0911
+
+Module::Pluggable::Dependency 0.0.3
+
+Regexp::Common::IRC 0.02
+
+Net::AIML 0.01
+
+HTML::Strip 1.06
+
+Bone::Easy 0.04
+
+=head1 AUTHOR
+
+Chris Williams <chris@bingosnet.co.uk>
+
+=cut
